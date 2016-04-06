b88587a4896f5e9316adf5f4082ba19628a1602a
Luis Diogo Couto <coutold@utrc.utc.com>
2016-04-06 18:35:25 +0100

    Add a gulp task for TS compilation.
    This task compiles the TS files into JS files and places them
    in the output folder. For now, it's called dist for distribution but
    we can change it if necessary.
    
    The task is configured to use the tsconfig files so we get the compiler
    errors. But sourcemaps have to be generated explicitly. The gulp plugin
    for typescript doesn't support sourcemaps directly. But there is another
    plugin that does so we just use that.
    
    Refs: #1

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,28 @@
 'use strict';
 
-var gulp = require('gulp');
+// Locations 
+var outputPath = 'dist/',
+    tsSrcs = 'src/**/*.ts';
+
+// Tools.
+var gulp = require('gulp'),
+    ts = require('gulp-typescript'),
+    sourcemap = require('gulp-sourcemaps'),
+    tsProject = ts.createProject('tsconfig.json'),
+
+// Tasks
+// Compile TS->JS with sourcemaps 
+gulp.task("compile-ts", function () {
+    var tsResult = gulp.src(tsSrcs)
+                        .pipe(sourcemap.init())
+                        .pipe(ts(tsProject));
+   
+    tsResult.dts.pipe(gulp.dest(outputPath));
+
+    return tsResult.js.pipe(sourcemap.write('.'))
+                          .pipe(gulp.dest(outputPath));
+});
+
 
 // Default task 
 gulp.task('default', function() { });
