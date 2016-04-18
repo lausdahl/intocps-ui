7a9b847af2f6ec81304bbe1c5ff8f2f3cfe94c50
Kenneth Lausdahl <lausdahl@eng.au.dk>
2016-04-18 02:33:01 +0200

    updated ui for COE so it can perform a simulation and upload fmus. It requires a development version of the COE since the session relative path must be fixed in the COE first

--- src/coe/coe.ts
+++ src/coe/coe.ts
@@ -10,10 +10,14 @@ class CoeController {
     resultCmdZipType: string = "zip";
     destroyCmd: string = "destroy";
 
+    uploadCmd: string = "upload/";
+
     configButton: HTMLButtonElement;
     remote: Electron.Remote;
     dialog: Electron.Dialog;
-    configFilePath: HTMLInputElement;
+    // configFilePath: HTMLInputElement;
+    projectRootPath: HTMLInputElement;
+    configFileName = "config.json";
 
     config: Object;
 
@@ -28,31 +32,83 @@ class CoeController {
     }
 
     initialize() {
-        this.configFilePath = <HTMLInputElement>document.getElementById("configFileText");
 
-        this.configFilePath.value = "/Users/kel/data/into-cps/intocps-coe/orchestration/coe/src/test/resources/online-models/watertank-c/config.json";
+        this.projectRootPath = <HTMLInputElement>document.getElementById("projectRootPathText");
+        this.projectRootPath.value = "/Users/kel/data/into-cps/intocps-ui/test-sim";
+        this.setProgress(0, null);
+
     }
 
-    launchExplorer() {
-
-        let dialogResult: string[] = this.dialog.showOpenDialog({ properties: ["openFile"] });
+    launchProjectExplorer() {
+        let dialogResult: string[] = this.dialog.showOpenDialog({ properties: ["openDirectory"] });
         if (dialogResult != undefined) {
-            this.configFilePath.value = dialogResult[0];
+            this.projectRootPath.value = dialogResult[0];
         }
+
     }
+
+    getConfigFile(): string {
+        return this.projectRootPath.value+"/" + this.configFileName;
+    }
+
 
     uploadFmus() {
         var _this = this;
-        setTimeout(function () {
 
-            var div = <HTMLInputElement>document.getElementById("coe-debug");
+        var div = <HTMLInputElement>document.getElementById("coe-debug");
 
-            div.innerHTML = "Fmus uploaded";
-            _this.setProgress(10);
+        var res = _this.parseConfig(_this.getConfigFile());
 
-            _this.initializeCoe();
+        var message = "Uploading Fmu: "
+        div.innerHTML = message;
+        _this.setProgress(_this.progressState, "Uploading Fmus");
 
-        }, 2000);
+        var formData = new FormData();
+
+        $.each(res.fmus, function (i, item) {
+
+            var path: string = item;
+            let SESSION = "session:/";
+
+            if (path.indexOf(SESSION) == 0) {
+                path = path.substring(SESSION.length);
+            }
+
+            message = message + path + ",";
+
+            div.innerHTML = message;
+            ///_this.setProgress(_this.progressState, message);
+
+            let filePath = _this.projectRootPath.value+"/" + path;
+
+            var content = _this.fs.readFileSync(filePath);
+            var blob = new Blob([content], { type: "multipart/form-data" });
+
+            formData.append('file', blob, path);
+
+
+
+        })
+
+        let url = _this.url + _this.uploadCmd + _this.sessionId;
+
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: formData,
+            processData: false,  // tell jQuery not to process the data
+            contentType: false,  // tell jQuery not to set contentType
+            success: function (data) {
+                console.log(data);
+                // alert(data);
+
+                _this.setProgress(50, "FMU upload done.");
+
+                _this.initializeCoe();
+            }
+        });
+
+
 
 
     }
@@ -65,39 +121,12 @@ class CoeController {
     initializeCoe() {
         var _this = this;
 
-        setTimeout(function () {
+        _this.setDebugMessage("Initializing the COE");
+        _this.setProgress(_this.progressState, "Initializing the COE");
 
-            _this.setDebugMessage("Initializing the COE");
+        var dat = JSON.stringify(_this.config);
+        let url = _this.url + _this.initializeSessionCmd + _this.sessionId;
 
-            var dat = JSON.stringify(_this.config);
-            let url = _this.url + _this.initializeSessionCmd + _this.sessionId;
-
-
-            jQuery.ajax({
-                url: url,
-                type: "POST",
-                data: dat,
-                dataType: "json",
-                contentType: "application/json; charset=utf-8",
-                success: function () {
-                    _this.setProgress(20);
-                    _this.start();
-                }
-            });
-
-
-        }, 2000);
-
-    }
-
-    start() {
-
-        var _this = this;
-
-        var dat = JSON.stringify({ startTime: 0, endTime: 10 });
-        let url = _this.url + _this.simulateCmd + _this.sessionId;
-
-        _this.setDebugMessage("Starting simulation");
 
         jQuery.ajax({
             url: url,
@@ -106,40 +135,68 @@ class CoeController {
             dataType: "json",
             contentType: "application/json; charset=utf-8",
             success: function () {
-                _this.setProgress(30);
-                _this.start();
+                _this.setProgress(50, "Initialization done.");
+                _this.simulate();
             }
-        }).fail(function(){
-            
-            console.error("error in simulation call");
-             _this.setDebugMessage("Starting simulation: FAILED");
         });
 
     }
 
     simulate() {
+
         var _this = this;
 
-        this.setProgress(0);
+        var dat = JSON.stringify({ startTime: 0, endTime: 10 });
+        let url = _this.url + _this.simulateCmd + _this.sessionId;
 
+        _this.setDebugMessage("Starting simulation");
+        _this.setProgress(_this.progressState, "Simulating");
 
-        this.fs.readFile(this.configFilePath.value, function (err: any, data: any) {
-            if (err) {
-                return console.error(err);
+        jQuery.ajax({
+            url: url,
+            type: "POST",
+            data: dat,
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function () {
+                _this.setProgress(100, "Simulation done.");
             }
-            console.log("Asynchronous read: " + data.toString());
-            var cfg = JSON.parse(data.toString());
-            console.log(cfg);
+        }).fail(function () {
 
-            _this.config = cfg;
+            console.error("error in simulation call");
+            _this.setDebugMessage("Starting simulation: FAILED");
+        });
 
-            var divCoeFmus = <HTMLInputElement>document.getElementById("coe-fmus");
-            $.each(cfg.fmus, function (i, item) {
-                var fmuBtn = <HTMLDivElement>document.createElement("div");
-                divCoeFmus.appendChild(fmuBtn);
-                fmuBtn.innerHTML = item;
+    }
 
-            })
+    parseConfig(path: string): any {
+
+        var content = this.fs.readFileSync(path, "utf8");
+        console.log("Asynchronous read: " + content.toString());
+        var cfg = JSON.parse(content.toString());
+        console.log(cfg);
+        return cfg;
+    }
+
+    launch() {
+        var _this = this;
+
+       // _this.setProgress(15, "Creating session");
+ _this.setProgress(0,null);
+ 
+        var cfg = _this.parseConfig(_this.getConfigFile());
+        if (cfg == null) {
+            return console.error("Unable to parse config file: " + _this.getConfigFile());
+
+        }
+
+        _this.config = cfg;
+
+        var divCoeFmus = <HTMLInputElement>document.getElementById("coe-fmus");
+        $.each(cfg.fmus, function (i, item) {
+            var fmuBtn = <HTMLDivElement>document.createElement("div");
+            divCoeFmus.appendChild(fmuBtn);
+            fmuBtn.innerHTML = item;
 
         });
 
@@ -156,18 +213,27 @@ class CoeController {
                 _this.sessionId = data.sessionId;
                 _this.setDebugMessage("Session created with id: " + data.sessionId);
 
-                _this.setProgress(5);
+                _this.setProgress(25,null);// "Session created");
                 _this.uploadFmus();
 
             });
 
     }
 
-    setProgress(progress: number) {
+    progressState: number = 0;
+
+    setProgress(progress: number, message: string) {
+
         var divProgress = <HTMLInputElement>document.getElementById("coe-progress");
         let tmp = progress.toString() + "%";
+
         divProgress.style.width = tmp;
-        divProgress.innerHTML = tmp;
+        if (message != null) {
+            divProgress.innerHTML = tmp + " - " + message;
+        } else {
+            divProgress.innerHTML = tmp;
+        }
+        this.progressState = progress;
     }
 
 }
