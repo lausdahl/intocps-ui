044f9f6ccddb913865d24138910f6db38ecc7466
Casper Thule <casper.thule@eng.au.dk>
2016-04-26 20:12:42 +0200

    Create settings and added to global object.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,86 @@
 'use strict';
 
 const electron = require('electron');
+const fs = require('fs');
+const path = require('path');
 // Module to control application life.
 const app = electron.app;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
+// Path to userData
+const userDataPath = app.getPath('userData')
+const intoCpsFolder = path.normalize(userDataPath + "/intocps-ui");
+const settingsFile = path.normalize(intoCpsFolder + "/settings.json");
+
+global.intoCps = {
+  data: {},
+  getDefaultDirectory: function () {
+    return userDataPath;
+  },
+  storeSettings: function () {
+    fs.open(settingsFile, "w", (err, fd) => {
+      if (err) {
+        "The error: " + err + " happened when attempting to open the file: " + settingsFile;
+      }
+      else {
+        fs.write(fd, JSON.stringify(global.intoCps.data), (err) => {
+          if (err) {
+            console.log("Failed to write settings to file: " + settingsFile);
+          }
+          fs.close(fd, (err) => {
+            if (err) {
+              console.log("Failed to close writing to the file: " + settingsFile);
+              throw err;
+            }
+          });
+        });
+      }
+    });
+  },
+  loadSettings: function () {
+    fs.readFile(settingsFile, (err, data) => {
+      if (!err) {
+        global.intoCps.data = JSON.parse(data);
+        console.log("Loaded settings: " + JSON.stringify(global.intoCps.data));
+      }
+      else {
+        console.log("The error: " + err + " happened when attempting to load the file: " + settingsFile);
+        throw err;
+      }
+    });
+  }
+}
+
+function initializeSettings() {
+  // Check if file exists
+  fs.lstat(settingsFile, function (err, data) {
+    if (err || !data.isFile()) {
+      console.log("Settings file does not exist. Creating " + settingsFile);
+      fs.mkdir(intoCpsFolder, (err) => {
+        if (err) {
+          console.log("The error: " + err + " happened when attempting to create the directory: " + intoCpsFolder);
+          throw err;
+        }
+        else {
+          global.intoCps.storeSettings();
+        }
+      });
+    }
+    else {
+      global.intoCps.loadSettings();
+    }
+  });
+}
+initializeSettings();
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
@@ -21,7 +89,7 @@ function createWindow () {
   mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
