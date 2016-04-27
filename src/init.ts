ee895ca3177ad49cf7439cacddb2f7b49804671a
Luis Diogo Couto <coutold@utrc.utc.com>
2016-04-27 12:26:15 +0100

    Switch the active marker when changing tabs.
    REFS: #10

--- src/init.ts
+++ src/init.ts
@@ -7,6 +7,17 @@ var mainViewId : string = "mainView";
 var coeController = new CoeController();
 var myLayout;
 
+
+/// Switch active tab marker
+$('.navbar li').click(function(e) {
+    $('.navbar li.active').removeClass('active');
+    var $this = $(this);
+    if (!$this.hasClass('active')) {
+        $this.addClass('active');
+    }
+});
+
+
 initLayout();
 
 
