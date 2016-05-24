5a6f7f0445a6ec4cd2cb4879f5154f91776ddaa7
Florian Lapschies <florian.lapschies@verified.de>
2016-05-24 19:30:03 +0200

    Refactor handling of context menus.
    Each ProjectBrowserItem now has an array of menu entries.

--- src/proj/projbrowserview.ts
+++ src/proj/projbrowserview.ts
@@ -13,6 +13,21 @@ import Path = require('path');
 import {IntoCpsAppMenuHandler} from "../IntoCpsAppMenuHandler";
 import {eventEmitter} from "../Emitter";
 
+export class MenuEntry {
+    id: string;
+    text: string;
+    icon: any;
+    item: ProjectBrowserItem;
+    callback: (item: ProjectBrowserItem) => void;
+    constructor(item: ProjectBrowserItem, text: string, icon: any, callback: (item: ProjectBrowserItem) => void) {
+        this.item = item;
+        this.id = text;
+        this.text = text;
+        this.icon = icon;
+        this.callback = callback;
+    }
+}
+
 export class ProjectBrowserItem {
     id: string;
     text: string;
@@ -25,6 +40,7 @@ export class ProjectBrowserItem {
 
     clickHandler(): void { }
     dblClickHandler(): void { }
+    menuEntries: MenuEntry[] = [];
 
     constructor(path: string, parent: ProjectBrowserItem) {
         this.id = path;
@@ -38,6 +54,12 @@ export class ProjectBrowserItem {
             this.level = parent.level + 1;
             parent.nodes.push(this);
         }
+    }
+    addMenuEntry(text: string, icon: any, callback: (item: ProjectBrowserItem) => void) {
+        this.menuEntries.push(new MenuEntry(this, text, icon, callback));
+    }
+    addMenuEntryObject(entry: MenuEntry) {
+        this.menuEntries.push(entry));
     }
     removeFileExtensionFromText(): void {
         this.text = this.text.substr(0, this.text.indexOf('.'));
@@ -53,19 +75,8 @@ export class ProjectBrowserItem {
 export class BrowserController {
     private browser: HTMLDivElement;
     private tree: W2UI.W2Sidebar;
-    private dblClickHandlers: Array<(event: JQueryEventObject) => void> = [];
 
     private menuHandler: IntoCpsAppMenuHandler = null;
-
-    private CTXT_DUBLICATE_ID: string = "Duplicate";
-    private CTXT_DELETE_ID: string = "Duplicate";
-    private CTXT_CREATE_MULTI_MODEL_ID: string = "create-multi-model";
-    private CTXT_CREATE_CO_SIM_CONFIG_ID: string = "create-co-sim-config";
-    private CTXT_IMPORT_ID: string = "import";
-    private CTXT_EXPORT_ID: string = "export";
-    private CTXT_CREATE_DSE_ID: string = "dse";
-    private CTXT_CREATE_TEST_DATA_GERATION_PROJECT: string = "create test data generation project";
-    private CTXT_CREATE_MODEL_CHECKING_PROJECT: string = "create model checking project";
 
     constructor(menuHandler: IntoCpsAppMenuHandler) {
         this.menuHandler = menuHandler;
@@ -76,124 +87,34 @@ export class BrowserController {
         this.browser = <HTMLDivElement>document.querySelector("#browser");
         let remote = require("remote");
 
-        let DEFAULT_MENU = [
-            // { id: this.CTXT_DUBLICATE_ID, text: "Duplicate", icon: 'glyphicon glyphicon-duplicate' },
-            { id: this.CTXT_DELETE_ID, text: "Delete", icon: 'glyphicon glyphicon-remove' },
-            // { id: this.CTXT_CREATE_MULTI_MODEL_ID, text: "Create Multi-Model", icon: 'glyphicon glyphicon-briefcase' },
-            // { id: this.CTXT_CREATE_CO_SIM_CONFIG_ID, text: "Create Co-Simulation Configuration", icon: 'glyphicon glyphicon-copyright-mark' },
-            { id: this.CTXT_IMPORT_ID, text: "Import", icon: 'glyphicon glyphicon-import' },
-            { id: this.CTXT_EXPORT_ID, text: "Export", icon: 'glyphicon glyphicon-export' },
-        ];
-
         this.tree = $(this.browser).w2sidebar({
             name: 'sidebar',
-            menu: DEFAULT_MENU
+            menu: []
         });
-
-        let MM_MENU = [
-            { id: this.CTXT_DUBLICATE_ID, text: "Duplicate", icon: 'glyphicon glyphicon-duplicate' },
-            { id: this.CTXT_DELETE_ID, text: "Delete", icon: 'glyphicon glyphicon-remove' },
-            { id: this.CTXT_CREATE_CO_SIM_CONFIG_ID, text: "Create Co-Simulation Configuration", icon: 'glyphicon glyphicon-copyright-mark' },
-            { id: this.CTXT_IMPORT_ID, text: "Import", icon: 'glyphicon glyphicon-import' },
-            { id: this.CTXT_EXPORT_ID, text: "Export", icon: 'glyphicon glyphicon-export' },
-        ];
-
-        let COSIM_MENU = [
-            { id: this.CTXT_DUBLICATE_ID, text: "Duplicate", icon: 'glyphicon glyphicon-duplicate' },
-            { id: this.CTXT_DELETE_ID, text: "Delete", icon: 'glyphicon glyphicon-remove' },
-            { id: this.CTXT_IMPORT_ID, text: "Import", icon: 'glyphicon glyphicon-import' },
-            { id: this.CTXT_EXPORT_ID, text: "Export", icon: 'glyphicon glyphicon-export' },
-        ];
-
-        let SYSML_EX_MENU = [
-            { id: this.CTXT_CREATE_MULTI_MODEL_ID, text: "Create Multi-Model", icon: 'glyphicon glyphicon-briefcase' },
-            { id: this.CTXT_DELETE_ID, text: "Delete", icon: 'glyphicon glyphicon-remove' },
-            { id: this.CTXT_IMPORT_ID, text: "Import", icon: 'glyphicon glyphicon-import' },
-            { id: this.CTXT_EXPORT_ID, text: "Export", icon: 'glyphicon glyphicon-export' },
-        ];
-
-        let DSE_MENU = [
-            { id: this.CTXT_CREATE_DSE_ID, text: "Create Design Space Exploration Config", icon: 'glyphicon glyphicon-asterisk' },
-        ];
-
-        let TEST_DATA_GENERATION_MENU = [
-            { id: this.CTXT_CREATE_TEST_DATA_GERATION_PROJECT, text: "Create Test Data Generation Project", icon: 'glyphicon glyphicon-asterisk' },
-        ];
-
-        let MODEL_CHECKING_MENU = [
-            { id: this.CTXT_CREATE_MODEL_CHECKING_PROJECT, text: "Create Model Checking Project", icon: 'glyphicon glyphicon-asterisk' },
-        ];
 
         this.tree.on("contextMenu", (event: any) => {
             console.log(event);
-            let id: String = event.target + "";
-            if (id.indexOf('mm.json') >= 0) {
-                this.tree.menu = MM_MENU;
-            } else if (id.indexOf('coe.json') >= 0) {
-                this.tree.menu = COSIM_MENU;
-            } else if (id.indexOf('sysml.json') >= 0) {
-                this.tree.menu = SYSML_EX_MENU;
-            } else if (Path.basename(id.toString()) == Project.PATH_TEST_DATA_GENERATION) {
-                this.tree.menu = TEST_DATA_GENERATION_MENU;
-            } else if (Path.basename(id.toString()) == Project.PATH_MODEL_CHECKING) {
-                this.tree.menu = MODEL_CHECKING_MENU;
-            } else if (Path.basename(id.toString()) == Project.PATH_DSE) {
-                this.tree.menu = DSE_MENU;
-            } else {
-                this.tree.menu = DEFAULT_MENU;
-            }
+            var item: ProjectBrowserItem = <ProjectBrowserItem>((<any>event).object);
+            var menu: MenuEntry[] = item.menuEntries;
+            this.tree.menu = menu;
         });
 
         this.tree.on("menuClick", (event: any) => {
-
-            var id: string = "";
-
-            if (event.menuItem != undefined) {
-                id = event.menuItem.id;
-            }
-
-            if (id == this.CTXT_CREATE_TEST_DATA_GERATION_PROJECT) {
-                console.info("Create new test data generation project");
-                this.menuHandler.createRTTesterProject(event.target);
-            } else if (id == this.CTXT_CREATE_MODEL_CHECKING_PROJECT) {
-                console.info("Create new model checking project");
-                this.menuHandler.createRTTesterProject(event.target);
-            } else if (id == this.CTXT_CREATE_DSE_ID) {
-                console.info("Create new DSE config ");
-                this.menuHandler.createDse(event.target);
-            } else if (id.indexOf(this.CTXT_CREATE_MULTI_MODEL_ID) == 0) {
-                if (event.target.indexOf('sysml.json') >= 0) {
-                    console.info("Create new multimodel for: " + event.target);
-                    this.menuHandler.createMultiModel(event.target + "");
-                } else {
-                    const { dialog } = require('electron').remote;
-                    dialog.showErrorBox("Cannot create multi-model", "A multi-model cannot be created based on the selected resource.")
-                }
-            } else if (id.indexOf(this.CTXT_CREATE_CO_SIM_CONFIG_ID) == 0) {
-                if (event.target.indexOf('mm.json') >= 0) {
-                    console.info("Create new cosim config for: " + event.target);
-                    this.menuHandler.createCoSimConfiguration(event.target + "");
-                } else {
-                    const { dialog } = require('electron').remote;
-                    dialog.showErrorBox("Cannot create co-simulation configuration", "A co-simulation configuration cannot be created based on the selected resource.")
-                }
-            } else if (id.indexOf(this.CTXT_DELETE_ID) == 0) {
-                let name = Path.basename(event.target);
-                if (name.indexOf('R_') >= 0) {
-                    console.info("Deleting " + event.target);
-                    this.getCustomFs().removeRecursive(event.target, function (err: any, v: any) {
-                        if (err != null) {
-                            console.error(err);
-                        }
-                        self.refreshProjectBrowser();
-                    });
-
-                }
-
-            }
+            var entry: MenuEntry = <MenuEntry>((<any>event).menuItem);
+            entry.callback(entry.item);
         });
 
-        this.addHandlers();
+        this.tree.on("dblClick", (event: JQueryEventObject) => {
+            //Remove auto expansion on double click
+            event.preventDefault();
+            var item: ProjectBrowserItem = <ProjectBrowserItem>((<any>event).object);
+            item.dblClickHandler();
+        });
+
+        this.tree.on("click", (event: JQueryEventObject) => {
+            var item: ProjectBrowserItem = <ProjectBrowserItem>((<any>event).object);
+            item.clickHandler();
+        });
 
         this.refreshProjectBrowser();
 
@@ -219,8 +140,8 @@ export class BrowserController {
 
 
     private addFSItem(path: string, parent: ProjectBrowserItem): ProjectBrowserItem {
-        var _this = this;
-        var result: ProjectBrowserItem = result = new ProjectBrowserItem(path, parent);
+        var self = this;
+        var result: ProjectBrowserItem = new ProjectBrowserItem(path, parent);
         var stat = fs.statSync(path);
         if (Path.basename(path).startsWith('.')) {
             return null;
@@ -232,7 +153,7 @@ export class BrowserController {
                 (<any>parent).coeConfig = path;
                 parent.removeNodeWithPath(path);
                 parent.dblClickHandler = function () {
-                    _this.menuHandler.openCoeView(path);
+                    self.menuHandler.openCoeView(path);
                 };
                 result = null;
             }
@@ -242,47 +163,82 @@ export class BrowserController {
                 (<any>parent).mmConfig = path;
                 parent.removeNodeWithPath(path);
                 parent.dblClickHandler = function () {
-                    _this.menuHandler.openMultiModel(path);
+                    self.menuHandler.openMultiModel(path);
                 };
+                parent.addMenuEntry("Create Co-Simulation Configuration", 'glyphicon glyphicon-copyright-mark',
+                    function (item: ProjectBrowserItem) {
+                        console.info("Create new cosim config for: " + item.id);
+                        self.menuHandler.createCoSimConfiguration(item.id);
+                    });
                 result = null;
             }
             else if (path.endsWith('.fmu')) {
                 result.img = 'icon-page';
                 result.removeFileExtensionFromText();
                 parent.dblClickHandler = function () {
-                    _this.menuHandler.openFmu(path);
+                    self.menuHandler.openFmu(path);
                 };
             }
             else if (path.endsWith('.sysml.json')) {
                 result.img = 'glyphicon glyphicon-tasks';
                 result.removeFileExtensionFromText();
                 result.dblClickHandler = function () {
-                    _this.menuHandler.openSysMlExport(path);
+                    self.menuHandler.openSysMlExport(path);
                 };
-                //children.push(new Container(name, filePath, ContainerType.SysMLExport));
+                result.addMenuEntry("Create Multi-Model", 'glyphicon glyphicon-briefcase',
+                    function (item: ProjectBrowserItem) {
+                        console.info("Create new multimodel for: " + item.id);
+                        self.menuHandler.createMultiModel(item.id);
+                    });
             }
             else if (path.endsWith('.emx')) {
                 result.img = 'glyphicon glyphicon-tree-conifer';
                 result.removeFileExtensionFromText();
-                //children.push(new Container(name, filePath, ContainerType.EMX));
             }
             else if (path.endsWith('.mo')) {
                 result.img = 'glyphicon glyphicon-tree-deciduous';
                 result.removeFileExtensionFromText();
-                //children.push(new Container(name, filePath, ContainerType.MO));
             }
             else if (path.endsWith('.csv')) {
                 result.img = 'glyphicon glyphicon-th-list';
                 result.removeFileExtensionFromText();
-                //children.push(new Container(name, filePath, ContainerType.CSV));
             }
         } else if (stat.isDirectory()) {
             result.img = 'icon-folder';
             if (this.isOvertureProject(path)) {
                 result.img = 'glyphicon glyphicon-leaf';
                 result.expanded = false;
-            } else if (name.indexOf("R_") == 0) {
+            }
+            else if (Path.basename(path) == Project.PATH_TEST_DATA_GENERATION) {
+                result.addMenuEntry("Create Test Data Generation Project", 'glyphicon glyphicon-asterisk',
+                    function (item: ProjectBrowserItem) {
+                        self.menuHandler.createRTTesterProject(item.id);
+                    });
+            }
+            else if (Path.basename(path) == Project.PATH_MODEL_CHECKING) {
+                result.addMenuEntry("Create Model Checking Project", 'glyphicon glyphicon-asterisk',
+                    function (item: ProjectBrowserItem) {
+                        self.menuHandler.createRTTesterProject(item.id);
+                    });
+            }
+            else if (Path.basename(path) == Project.PATH_DSE) {
+                result.addMenuEntry("Create Design Space Exploration Config", 'glyphicon glyphicon-asterisk',
+                    function (item: ProjectBrowserItem) {
+                        self.menuHandler.createDse(item.id);
+                    });
+            }
+            else if (name.indexOf("R_") == 0) {
                 result.img = 'glyphicon glyphicon-barcode';
+                result.addMenuEntry("Delete", 'glyphicon glyphicon-remove',
+                    function (item: ProjectBrowserItem) {
+                        console.info("Deleting " + event.target);
+                        this.getCustomFs().removeRecursive(event.target, function (err: any, v: any) {
+                            if (err != null) {
+                                console.error(err);
+                            }
+                            self.refreshProjectBrowser();
+                        });
+                    });
             }
             var children: ProjectBrowserItem[] = this.addFSFolderContent(path, result);
         }
@@ -292,10 +248,10 @@ export class BrowserController {
     private addFSFolderContent(path: string, parent: ProjectBrowserItem = null): ProjectBrowserItem[] {
         var result: ProjectBrowserItem[] = [];
         var fs = require('fs');
-        var _this = this;
+        var self = this;
         fs.readdirSync(path).forEach(function (name: string) {
             var filePath: string = Path.join(path, name);
-            var ret = _this.addFSItem(filePath, parent);
+            var ret = self.addFSItem(filePath, parent);
             if (ret != null) {
                 result.push(ret);
             }
@@ -316,20 +272,6 @@ export class BrowserController {
             return value.id
         });
         this.tree.remove.apply(this.tree, ids);
-    }
-
-    private addHandlers() {
-        this.tree.on("dblClick", (event: JQueryEventObject) => {
-            //Remove auto expansion on double click
-            event.preventDefault();
-            var item: ProjectBrowserItem = <ProjectBrowserItem>((<any>event).object);
-            item.dblClickHandler();
-        });
-
-        this.tree.on("click", (event: JQueryEventObject) => {
-            var item: ProjectBrowserItem = <ProjectBrowserItem>((<any>event).object);
-            item.clickHandler();
-        });
     }
 
     getSelectedId(): string {
